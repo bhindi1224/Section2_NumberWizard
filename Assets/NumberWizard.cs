836f7dcf7e2349ea3aaec402b42237b5c35b541d
James Poore <jamie1224@gmail.com>
2018-07-26 22:05:48 -0400

    Lecture 15 Functions & Encapsulating

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour {
 
-        int max = 1000;
-        int min = 1;
-        int guess = 500;
+        int max;
+        int min;
+        int guess;
 
 	// Use this for initialization
 	void Start ()
     {
+        StartGame();
+	}
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
 
         Debug.Log("Welcome to Number Wizard.");
         Debug.Log("Pick a number between " + min + " and " + max + ".");
@@ -20,7 +28,7 @@ public class NumberWizard : MonoBehaviour {
         Debug.Log("   Down Arrow if your number is lower,");
         Debug.Log("   Enter if I guessed correctly.");
         max += 1;
-	}
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -29,19 +37,24 @@ public class NumberWizard : MonoBehaviour {
         {
             Debug.Log("Ok, my guess was too low.");
             min = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is your number " + guess + "?");
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Alright, my guess was too high.");
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is your number " + guess + "?");
+            NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Great, I knew your number was " + guess + " all along!");
+            StartGame();
         }
+    }
+
+    void NextGuess()
+    {
+            guess = (max + min) / 2;
+            Debug.Log("Is your number " + guess + "?");
     }
 }
