82501abb6739d2e94e1f646f5ca6d26140da5eab
James Poore <jamie1224@gmail.com>
2018-07-26 20:38:40 -0400

    Lecture 9 Print to console with Debug.Log

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Welcome to Number Wizard, yo");
+        Debug.Log("Pick a number.");
+        Debug.Log("The highest number is: 1000");
+        Debug.Log("The lowest number is: 1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
