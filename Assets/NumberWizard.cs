e31f6f74858916e8a655e8714a4ec0577ba1262d
James Poore <jamie1224@gmail.com>
2018-07-26 20:51:31 -0400

    Lecture 10 Introducing Variables

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -6,10 +6,13 @@ public class NumberWizard : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        int max = 1000;
+        int min = 1;
+
         Debug.Log("Welcome to Number Wizard, yo");
         Debug.Log("Pick a number.");
-        Debug.Log("The highest number is: 1000");
-        Debug.Log("The lowest number is: 1");
+        Debug.Log("The highest number is: " + max);
+        Debug.Log("The lowest number is: " + min);
 	}
 	
 	// Update is called once per frame
